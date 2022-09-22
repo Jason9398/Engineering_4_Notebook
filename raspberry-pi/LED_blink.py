22b770e26db2fbf23bc58113c53ba47afcb26a56
Jason9398 <Jmarshm76@charlottesvilleschools.org>
2022-09-22 13:45:10 -0400

    fdzf

---
+++ raspberry-pi/LED_blink.py
@@ -0,0 +1,9 @@
+import time
+import board 
+import digitalio 
+led = digitalio.DigitalInOut(board.LED) 
+led.direction = digitalio.Direction.OUTPUT 
+led.value = True  
+time.sleep(1)
+led.value = False
+time.sleep(1)
