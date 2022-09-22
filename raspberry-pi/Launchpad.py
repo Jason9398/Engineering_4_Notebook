fa579b1bd2aa41f8cf4d48b41c98fa8245efd0d7
Jason9398 <Jmarshm76@charlottesvilleschools.org>
2022-09-22 14:11:32 -0400

    thr

--- raspberry-pi/Launchpad.py
+++ raspberry-pi/Launchpad.py
@@ -1,7 +1,19 @@
 import time
-
+import board 
+import digitalio 
+led = digitalio.DigitalInOut(board.GP13) 
+led.direction = digitalio.Direction.OUTPUT
+ledgreen = digitalio.DigitalInOut(board.GP18) 
+ledgreen.direction = digitalio.Direction.OUTPUT 
 for x in range(10,0,-1):
   print(x)
-  time.sleep(1)
+led.value = True  
+time.sleep(.5)
+led.value = False
 
+ 
 print("lift off")
+ledgreen.value = True  
+time.sleep(.5)
+ledgreen.value = False
+
