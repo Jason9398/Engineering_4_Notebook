0102ae59fcef93f6663ab8ce261d632d0e4b18df
Jason9398 <Jmarshm76@charlottesvilleschools.org>
2022-09-27 14:17:55 -0400

    ty,kmnb

--- raspberry-pi/Launch Pad part 3.py
+++ raspberry-pi/Launch Pad part 3.py
@@ -4,19 +4,22 @@ import digitalio
 led = digitalio.DigitalInOut(board.GP13) 
 led.direction = digitalio.Direction.OUTPUT
 ledgreen = digitalio.DigitalInOut(board.GP18) 
-ledgreen.direction = digitalio.Direction.OUTPUT 
-butten,direction = digitalio.Direction.OUTPUT 
-butten.pull = digitalio.Pull.DOWN
-for x in range(10,0,-1):
-  print(x)
-led.value = True  
-time.sleep(1)
-led.value = False
+ledgreen.direction = digitalio.Direction.OUTPUT
+button = digitalio.DigitalInOut(board.GP16)  
+button.direction = digitalio.Direction.INPUT
+button.pull=digitalio.Pull.DOWN
 
-print("lift off")
-ledgreen.value = True  
-time.sleep(1)
-ledgreen.value = False
-butten,direction = digitalio.Direction.OUTPUT 
-butten.pull = digitalio.Pull.DOWN
-#print(butten.value)
+
+if button.value == True:
+    for x in range(10,0,-1):
+        print(x)
+        led.value = True  
+        time.sleep(1)
+    led.value = False
+
+    print("lift off")
+    ledgreen.value = True  
+    time.sleep(1)
+    ledgreen.value = False
+
+      #print(butten.value)P
