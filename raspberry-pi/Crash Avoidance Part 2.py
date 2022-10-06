32146db920f808f2ee81ccc858b797ef63c1da3d
Jason9398 <Jmarshm76@charlottesvilleschools.org>
2022-10-06 14:04:12 -0400

    hgmc

---
+++ raspberry-pi/Crash Avoidance Part 2.py
@@ -0,0 +1,18 @@
+import time 
+import board
+import adafruit_mpu6050 
+import busio
+sda_pin =board.GP16
+scl_pin =board.GP17
+i2c = busio.I2C(scl_pin, sda_pin)
+mpu = adafruit_mpu6050.MPU6050(i2c)
+led = digitalio.DigitalInOut(board.GP13) 
+led.direction = digitalio.Direction.OUTPUT
+while True:
+    print(mpu.acceleration)
+    time.sleep(1)
+    if mpu.acceleration[0] > 9:
+        #do stuff 
+        led.value = True 
+        time.sleep(1) 
+        led.value = False
