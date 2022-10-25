77dcea866dd251a87fd8ae09a4210ed67c01ddc8
Jason9398 <Jmarshm76@charlottesvilleschools.org>
2022-10-25 14:18:06 -0400

    POIUYTREWS

--- raspberry-pi/landing area part 1.py
+++ raspberry-pi/landing area part 1.py
@@ -3,42 +3,44 @@ def triangle_area(x1y1, x2y2, x3y3):
     try:
         # separate each entry into a list
         x1y1 =x1y1.split(",")
-        x2y2 = (",")
-        x3y3 = (",")
+        x2y2 = x2y2.split(",")
+        x3y3 = x3y3.split(",")
 
 
         # pull cordinates out of each list and change them from strings to floats\
-        x1 =x1y1[0]
-        y1 =x1y1[1]
-        x2 =-x2y2[0]
-        y2 =x2y2[1]
-        x3 =x3y3[0]
-        y3 =x3y3[1]
+        x1 =float(x1y1[0])
+        y1 =float(x1y1[1])
+        x2 =float(x2y2[0])
+        y2 =float(x2y2[1])
+        x3 =float(x3y3[0])
+        y3 =float(x3y3[1])
 
         # calculate area
-        area =12
+        area=0.5*abs(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))
+ 
+
 
         return area
 
     except:
-        print()  
+        print("error")  
         area = 0
         return area 
 
 #code to run in a loop
 while True:
 
- #request input
- val1 = input("input first set of coords:")
- val2 =input("input secand set of coords:")
- val3 =input("input third set of coords:")
- # run function and save output to areas variable
- area =triangle_area(val1,val2,val3)
- #if error in function,skip printed output and start loop over
- if area == 0:
-    continue
+    #request input
+    val1 = input("input first set of coords:")
+    val2 =input("input secand set of coords:")
+    val3 =input("input third set of coords:")
+    # run function and save output to areas variable
+    area =triangle_area(val1,val2,val3)
+    #if error in function,skip printed output and start loop over
+    if area == 0:
+        continue
 
 
-#if no error,print the area message and start loop over
-else:
-    print() 
+    #if no error,print the area message and start loop over
+    else:
+        print(area) 
