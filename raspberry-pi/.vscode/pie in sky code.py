d1d69ee40938cdd9bd6059a687f5e36a29232df6
Jason9398 <Jmarshm76@charlottesvilleschools.org>
2023-01-10 14:16:45 -0500

    qwe

--- raspberry-pi/.vscode/pie in sky code.py
+++ raspberry-pi/.vscode/pie in sky code.py
@@ -8,9 +8,11 @@ import adafruit_displayio_ssd1306
 import terminalio
 import displayio
 displayio.release_displays()
+#set up i2c
 sda_pin =board.GP16
 scl_pin =board.GP17
 i2c = busio.I2C(scl_pin, sda_pin)
+#setting up display
 display_bus = displayio.I2CDisplay(i2c, device_address=0x3d, reset=board.GP0)
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 mpu = adafruit_mpu6050.MPU6050(i2c, address=0x68)
